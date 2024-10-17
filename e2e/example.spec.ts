47525acdd383a274319316e1028d814047e83395
Allan MacDonald <amacdonald@cgsinc.ca>
2024-10-17 17:33:45 -0300

    modified tagged tests to e2e

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title', { tag: '@DemoTest'} ,  async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link', { tag: '@DemoTest'} , async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -16,3 +16,14 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+// test('Backend Test - Auth EP is up', { 
+//   tag: ['@ProjectName', '@Tests', '@AuthUser', '@API', '@BackendTest'] 
+// }, async ({ request }) => {
+//   const response = await request.get(`/auth/actuator/health`);
+//   const body = await response.json();
+
+//   // Assertion
+//   expect(response.status()).toBe(200);
+//   expect(body.status).toBe("UP");
+// });
