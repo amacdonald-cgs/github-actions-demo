aadcf4fcf9b3013fd56a2909cbcaa23918fdde11
Allan MacDonald <amacdonald@cgsinc.ca>
2024-11-08 13:09:07 -0400

    Updates to variables

--- tests/health_checks.spec.ts
+++ tests/health_checks.spec.ts
@@ -2,12 +2,18 @@ import { test, expect } from '@playwright/test';
 import { checkServices } from '../test-helpers/healthChecks';
 
 test('Check API services', { tag: '@health' }, async () => {
+  const apiKey = process.env.HEYGEN_API_KEY;
+  expect(apiKey).toBeDefined();
+  const heyGenCriticalThreshold: string = process.env.HEYGEN_CRITICAL_THRESHOLD || '9000';
+  const heyGenWarningThreshold: string = process.env.HEYGEN_WARNING_THRESHOLD || '20000';
+
+
   const services = [
     {
       name: 'Heygen Service',
       config: {
         endpointUrl: 'https://api.heygen.com/v2/user/remaining_quota',
-        apiKey: process.env.HEYGEN_API_KEY || '',
+        apiKey: apiKey || '',
       },
     },
     // Add more services here...
@@ -24,6 +30,6 @@ test('Check API services', { tag: '@health' }, async () => {
     // Add more expected results here...
   ]);
   // the payload for Heygen Service should have a 'remaining_quota' property with a value no less than 30000
-  expect(results[0].payload.remaining_quota).toBeGreaterThanOrEqual(9000);
+  expect(results[0].payload.remaining_quota).toBeGreaterThanOrEqual(Number(heyGenCriticalThreshold));
 
 });
